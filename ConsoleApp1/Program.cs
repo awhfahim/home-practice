304bde4bf9695a1916239b9af6bd64fb87e473fc
Abu Wabaider Hasan Fahim <fahim.hasan112@outlook.com>
2022-01-06 20:44:52 +0600

    practice

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace usingif
+{
+    public class shift
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            if (a * 20 == 20)
+                Console.WriteLine("a is integer number");
+        }
+    }
+}

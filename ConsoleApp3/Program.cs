f9a733c9bf96afb5a3ce33a34beefbc9c15d2069
Abu Wabaider Hasan Fahim <fahim.hasan112@outlook.com>
2022-01-07 10:08:57 +0600

    Number Value Between Two Numbers

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ifelseprac
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num - 10 == 2)
+            {
+                Console.WriteLine("The Statement is true");
+            }
+            else
+            {
+                Console.WriteLine("The Statement is false");
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace NumberValuebtw2num
+{
+    public static class NumberValueExtensions
+    {
+        public static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 30;
+            if (a > b)
+            {
+                Console.WriteLine("A is greater than B");
+            }
+            else
+            {
+                Console.WriteLine("B is Greater than A");
+            }
+        }
+    }
+}

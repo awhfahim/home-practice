93a2e1b1800895c7c0982a29ce64f66af33cc62a
Abu Wabaider Hasan Fahim <fahim.hasan112@outlook.com>
2022-01-07 12:19:02 +0600

    Leap Year Program

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace leapyear
+{
+    public static class leapyear
+    {
+     public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year");
+            int year = int.Parse(Console.ReadLine());
+            if (year % 400 == 0)
+            {
+                Console.WriteLine("It's a Leap Year");
+            }
+            else if(year%4==0 && year%100 != 0)
+            {
+                Console.WriteLine("its a leap year");
+            }
+            else
+            {
+                Console.WriteLine("its not a Leap year");
+            }
+        }    
+    }
+}

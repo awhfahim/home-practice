9f6c5c65944addc7c9527efac47881342e913c98
Abu Wabaider Hasan Fahim <fahim.hasan112@outlook.com>
2022-01-07 16:59:03 +0600

    app 11, 12

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace alphabetcheck
+{
+    public static class alphabetcheck
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character");
+            char ch =Convert.ToChar(Console.ReadLine());
+            if((ch>='a' && ch<='z') || (ch >='A' && ch <='Z'))
+            {
+                Console.WriteLine("this is alphabet");
+            }
+            else
+            {
+                Console.WriteLine("this is not alphabet");
+            }
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace voweldeclare
+{
+    public static class voweldeclare
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a alphabet");
+            char ch=Convert.ToChar(Console.ReadLine());
+            if(ch=='a' || ch=='A' || ch=='e' || ch=='E' || ch=='i' || ch=='I' || ch=='o' || ch=='O' || ch=='u' || ch == 'U')
+            {
+                Console.WriteLine(" its a vowel");
+            }
+            else if(ch>='a' && ch<='z' || ch>='A' && ch <= 'Z')
+            {
+                Console.WriteLine("This is a Consonant");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+
+
+            
+        }
+    }
+}

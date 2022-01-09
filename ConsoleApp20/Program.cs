c62feca97b617b6876c3111ce5bdf7505b15a66c
Abu Wabaider Hasan Fahim <fahim.hasan112@outlook.com>
2022-01-09 14:58:56 +0600

    ok

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace way2
+{
+    public static class way2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("size of row");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("size of column");
+            int y = int.Parse(Console.ReadLine());
+            int[,] rwclmn = new int[x, y];
+            {
+                for(int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                        rwclmn[i, j] =int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("The sum of Row is");
+            for(int i = 0; i < x; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < y; j++)
+                {
+                    sum += rwclmn[i, j];
+                }
+                Console.WriteLine(sum);
+
+                    
+            }
+        }
+    }
+}

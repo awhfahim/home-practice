cb20c76ccd079c99ed623da132d1c9c689717def
Abu Wabaider Hasan Fahim <fahim.hasan112@outlook.com>
2022-02-17 11:29:15 +0600

    new code

---
+++ 977A/158A/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class A158
+{
+    public static void Main(string[] args)
+    {
+        string[] F_Input = Console.ReadLine().Split(' ');
+        int a = int.Parse(F_Input[0]);
+        int b = int.Parse(F_Input[1]);
+        int count = 0;
+        int sum = 0;
+
+        string[] S_Input = Console.ReadLine().Split(' ');
+        int[] array = new int[a];
+        for(int i = 0; i<a; i++)
+        {
+            array[i] = int.Parse(S_Input[i]);
+        }
+        for(int i = 0; i < a-1; i++)
+        {
+            int m = array[i] + array[i + 1];
+             sum +=  m;
+        }
+        if (array.Length == 1)
+        {
+            if (array[0] > 0)
+            {
+                count = 1;
+            }
+        }
+        else if (sum == 0)
+        {
+            count = 0;
+        }
+        else if(array[b-1] == 0)
+        {
+            for (int j = 0; j < a; j++)
+                if (array[b-1] < array[j])
+                    count++;
+        }
+        else if (array[b-1] > 0)
+        {
+            for (int j = 0; j < a; j++)
+            {
+                if (array[b - 1] == array[j] || array[b - 1] < array[j])
+                {
+                    count++;
+                }
+            }
+        }
+       
+        Console.WriteLine(count);
+    }
+}
---
+++ 977A/339B/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class B339
+{
+    public static void Main(string[] args)
+    {
+        string[] F_Input = Console.ReadLine().Split(' ');
+
+        long n = int.Parse(F_Input[0]);
+        long m = int.Parse(F_Input[1]);
+
+        string[] S_Input = Console.ReadLine().Split(' ');
+        long[] array = new long[m];
+        for (long i = 0; i < m; i++)
+        {
+            array[i] = Convert.ToInt64(S_Input[i]);
+        }
+
+        long y = 0;
+        long x = 0;
+        long sum = 0;
+
+        for (long i = 0; i < m - 1; i++)
+        {
+            y = array[0] - 1;
+            if (array[i] > array[i + 1])
+            {
+                long subs = n - array[i];
+                x = array[i + 1]-0;
+                sum += subs + x;
+            }
+            else
+            {
+                x = (array[i+1] - array[i]);
+                sum += x;
+            }
+        }
+        Console.WriteLine(sum + y);
+
+    }
+}
+

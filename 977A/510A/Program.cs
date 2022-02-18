64a6acc466c84f3f70d742eaa982ed72dae0aa78
Abu Wabaider Hasan Fahim <fahim.hasan112@outlook.com>
2022-02-19 01:56:48 +0600

    new Code

---
+++ 977A/510A/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class A510
+{
+    public static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int row = int.Parse(input[0]);
+        int Colm = int.Parse(input[1]);
+
+
+        for (int i = 1; i<=row; i++)
+        {
+            if (i % 2 != 0)
+            {
+                for(int j=0; j<Colm; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            else if (i % 4 == 0)
+            {
+                Console.Write("#");
+                for (int j = 0; j < Colm - 1; j++)
+                {
+                    Console.Write(".");
+                }
+                Console.WriteLine();
+            }
+            else if (i % 2 == 0)
+            {
+                for (int j = 0; j < Colm-1; j++)
+                {
+                    Console.Write(".");
+                }
+                Console.Write("#");
+                Console.WriteLine();
+            }
+                
+        }
+
+    }
+}

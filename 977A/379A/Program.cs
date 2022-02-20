facb7bff42ad2cc67f2e9bf9bf04b26af205506a
Abu Wabaider Hasan Fahim <fahim.hasan112@outlook.com>
2022-02-20 11:40:20 +0600

    new code

---
+++ 977A/379A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class A379
+{
+    public static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+
+        int NN = int.Parse(input[0]);
+        int N_Candle = int.Parse(input[0]);   
+        int W_Candle = int.Parse(input[1]);   
+        bool Condition = true;
+        int Result = 0;
+
+        while (Condition)
+        {
+            int a = N_Candle / W_Candle;  
+            int e = N_Candle % W_Candle;  
+            int sum = a + e;
+            if (sum < W_Candle)
+                Condition = false;
+
+            Result += a;
+            N_Candle = sum;
+        }
+        Console.WriteLine(Result+NN);
+    }
+}
---
+++ Simple Recursive Method/Simple Recursive Method/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Recursive
+{
+    internal static int Factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        return n*Factorial(n-1);
+    }
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        Console.WriteLine(Factorial(num));
+    }
+}

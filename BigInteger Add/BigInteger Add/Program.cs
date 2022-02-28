d9ea3906ccc2cb0b9ad96299172afb4662834db8
Abu Wabaider Hasan Fahim <fahim.hasan112@outlook.com>
2022-02-28 11:35:24 +0600

    BigInteger And NumberConversion

---
+++ BigInteger Add/BigInteger Add/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+class Big_integer
+{
+    public static string BigSum(string a,string b)
+    {
+        int a_idx1 = Convert.ToInt32(a[0]);
+        int b_idx1 = Convert.ToInt32(b[0]);
+        if (a.Length < b.Length)
+        {
+            string temp = a;
+            a = b;
+            b = temp;
+        }
+        else if (a.Length == b.Length && b_idx1 > a_idx1)
+        {
+            string temp = a;
+            a = b;
+            b = temp;
+        }
+
+        int plus = 0;
+        int x = a.Length - b.Length;
+        string[] charC = new string[x + b.Length];
+        for (int q = 0; q < x + b.Length; q++)
+        {
+            if (q < x)
+            {
+                charC[q] = "0";
+            }
+            else
+            {
+                charC[q] = Convert.ToString(b[plus]);
+                plus++;
+            }
+        }
+
+        int i = 0;
+        int j = 0;
+
+        string[] charA = new string[a.Length];
+        foreach (char c in a)
+        {
+            charA[i] = Convert.ToString(c);
+            i++;
+        }
+
+        int[] up = new int[a.Length];
+        for (int l = 0; l < a.Length; l++)
+        {
+            up[l] = int.Parse(charA[l]);
+        }
+
+        int[] down = new int[charC.Length];
+        for (int l = 0; l < charC.Length; l++)
+        {
+            down[l] = int.Parse(charC[l]);
+        }
+
+        string[] arr = new string[charA.Length];
+        for (int m = a.Length - 1; m > -1; m--)
+        {
+            if (up[m] < down[m])
+            {
+                up[m] = up[m] + 10;
+                arr[m] = Convert.ToString(up[m] - down[m]);
+                down[m - 1] = down[m - 1] + 1;
+            }
+            else
+            {
+                arr[m] = Convert.ToString(up[m] - down[m]);
+            }
+        }
+        string t = string.Empty;
+        foreach (string c in arr)
+            t = t + c;
+        return t;
+    }
+    public static void Main(string[] args)
+    {
+        string a = Console.ReadLine();
+        Console.WriteLine("(-)");
+        string b = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.Write("Answer is - ");
+        Console.WriteLine(BigSum(a,b));
+    }
+}
---
+++ NumberConversion/NumberConversion/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class Number_Conversion
+{
+    public static void Main(string[] args)
+    {
+        string Digits = Console.ReadLine();
+        double current_base = int.Parse(Console.ReadLine());
+        int Convert_base = int.Parse(Console.ReadLine());
+
+        int i = 0;
+        string[] arr = new string[Digits.Length];
+        foreach(char c in Digits)
+        {
+            arr[i] = Convert.ToString(c);
+            i++;
+        }
+        int j = 0;
+        while (j < arr.Length)
+        {
+            switch (arr[j])
+            {
+                case "A":
+                    arr[j] = "10";
+                    break;
+                case "B":
+                    arr[j] = "11";
+                    break;
+                case "C":
+                    arr[j] = "12";
+                    break;
+                case "D":
+                    arr[j] = "13";
+                    break;
+                case "E":
+                    arr[j] = "14";
+                    break;
+                case "F":
+                    arr[j] = "15";
+                    break;
+                default:
+                    arr[j] = arr[j];
+                    break;
+            }
+            j++;
+        }
+        int m = 0;
+        double sum = 0;
+        int power = arr.Length - 1;
+        while (m < arr.Length)
+        {
+            double value = int.Parse(arr[m]) * Math.Pow(current_base, power);
+            sum += value;
+            m++;
+            power--;
+        }
+        string Answer = string.Empty;
+        while(sum > 0)
+        {
+            double D_res = sum / Convert_base;
+            double D_end = sum % D_res;
+            sum = D_res;
+            Answer += Convert.ToString(D_end);
+        }
+        Console.WriteLine(Answer);
+    }
+}
